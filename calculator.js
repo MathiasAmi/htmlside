7931560837c3db2e427dae5b92df2e2cfb9e4836
MathiasAmi <math35i0@edu.easj.dk>
2019-09-18 11:20:25 +0200

    Added calculator page

---
+++ calculator.js
@@ -0,0 +1,31 @@
+
+function plus(){
+  let a = document.getElementById("a");
+  let b = document.getElementById("b");
+  let c = document.getElementById("c");
+  
+  c.value = parseInt(a.value) + parseInt(b.value);
+}
+  
+  function minus(){
+  let a = document.getElementById("a");
+  let b = document.getElementById("b");
+  let c = document.getElementById("c");
+  
+  c.value = parseInt(a.value) - parseInt(b.value);
+  }
+    function gange(){
+  let a = document.getElementById("a");
+  let b = document.getElementById("b");
+  let c = document.getElementById("c");
+  
+  c.value = parseInt(a.value) * parseInt(b.value);
+    }   
+      function dividere(){
+  let a = document.getElementById("a");
+  let b = document.getElementById("b");
+  let c = document.getElementById("c");
+  
+  c.value = parseInt(a.value) / parseInt(b.value);
+  
+}
